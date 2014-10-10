3b1c2e936f888c4171350b78c9495a7655c373f0
Justin Ryder <justin@justin-ryder.com>
2014-10-10 18:25:19 -0400

    added project structure.

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'app/css'
+sass_dir = 'src/sass'
+images_dir = 'src/images'
+javascripts_dir = 'src/js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
