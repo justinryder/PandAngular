307437c7416d8d0e57af1460a00512092fd7814a
Justin Ryder <justin@justin-ryder.com>
2014-10-10 23:57:05 -0400

    switched to area spline graph

--- src/js/graphController.js
+++ src/js/graphController.js
@@ -1,7 +1,12 @@
 app.controller('graphController', ['$scope', function($scope) {
   $scope.data = {
     columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]};
+      ['data1', 30, 200, 100, 400, 150, 250],
+      ['data2', 50, 20, 10, 40, 15, 25]
+    ],
+    types: {
+      data1: 'area-spline',
+      data2: 'area-spline'
+    }
+  };
 }]);
