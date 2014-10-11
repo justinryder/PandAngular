114aa975dd19cfdf204eabd80cf5cff84c8750de
Justin Ryder <justin@justin-ryder.com>
2014-10-10 23:54:01 -0400

    added c3.js and implemented their demo chart.

--- src/js/graphController.js
+++ src/js/graphController.js
@@ -1,3 +1,7 @@
 app.controller('graphController', ['$scope', function($scope) {
-  $scope.data = [4, 8, 15, 16, 23, 42];
+  $scope.data = {
+    columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]};
 }]);
--- src/js/graphDirective.js
+++ src/js/graphDirective.js
@@ -5,31 +5,10 @@ app.directive('graph', [function($window) {
     link: function(scope, elem, attrs){
       var data = scope.data;
 
-      var width = 420,
-          barHeight = 20;
-
-      var x = d3.scale.linear()
-          .domain([0, d3.max(data)])
-          .range([0, width]);
-
-      var chart = d3.select(".graph")
-          .attr("width", width)
-          .attr("height", barHeight * data.length);
-
-      var bar = chart.selectAll("g")
-          .data(data)
-        .enter().append("g")
-          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-      bar.append("rect")
-          .attr("width", x)
-          .attr("height", barHeight - 1);
-
-      bar.append("text")
-          .attr("x", function(d) { return x(d) - 3; })
-          .attr("y", barHeight / 2)
-          .attr("dy", ".35em")
-          .text(function(d) { return d; });
-          }
-        };
+      var chart = c3.generate({
+        bindto: '#' + elem.attr('id'),
+        data: data
+      });
+    }
+  };
 }]);
