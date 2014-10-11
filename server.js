7ee76b9c05bb5020d07075d9c97fa74a4f85ec76
Justin Ryder <justin@justin-ryder.com>
2014-10-10 20:09:39 -0400

    added mongoose connection logic.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
 
 var appDirectory = path.join(__dirname, 'app');
 app.use(express.static(appDirectory));
@@ -9,3 +10,13 @@ console.log('server using static ' + appDirectory);
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server listening on port ' + port);
+
+var dbPath = 'mongodb://localhost/panda-stats';
+mongoose.connect(dbPath);
+var db = mongoose.connection;
+db.on('error', function(){
+  console.error('error connecting to ' + dbPath);
+});
+db.on('open', function(){
+  console.log('connected to ' + dbPath);
+});
